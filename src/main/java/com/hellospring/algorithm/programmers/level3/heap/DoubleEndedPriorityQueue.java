5c6f5d6474e813db8157aa95e8eaec2650bd6593
hsbae <hellospring89@gmail.com>
2026-03-11 23:56:00 +0900

    힙 : 이중우선순위큐 문제풀이(초안)

--- src/main/java/com/hellospring/algorithm/programmers/level3/heap/DoubleEndedPriorityQueue.java
+++ src/main/java/com/hellospring/algorithm/programmers/level3/heap/DoubleEndedPriorityQueue.java
@@ -1,13 +1,40 @@
 package com.hellospring.algorithm.programmers.level3.heap;
 
-import com.hellospring.algorithm.programmers.level2.queue.FunctionDevelopment;
+import com.hellospring.basicClass.DualPriorityQueue;
 
 import java.util.Arrays;
 
 public class DoubleEndedPriorityQueue {
     public int[] solution(String[] operations) {
-        int[] answer = {};
-        return answer;
+        DualPriorityQueue dualPriorityQueue = new DualPriorityQueue();
+        int minValue = 0;
+        int maxValue = 0;
+
+        for (int i=0; i<operations.length; i++)
+        {
+            String[] list = operations[i].split(" ");
+            String firstValue = list[0];
+            int secondValue =  Integer.parseInt(list[1]);
+            if (firstValue.equals("I")) {
+                dualPriorityQueue.insert(secondValue);
+            }
+            else {
+                if(secondValue == 1) {
+                    dualPriorityQueue.deleteMax();
+                }
+                else {
+                    dualPriorityQueue.deleteMin();
+                }
+            }
+        }
+
+        if (!dualPriorityQueue.isEmpty()) {
+            maxValue = dualPriorityQueue.getMax();
+            minValue = dualPriorityQueue.getMin();
+        }
+
+
+        return new int[]{maxValue, minValue};
     }
 
     public static void main(String[] args) {
